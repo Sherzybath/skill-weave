fe5bb9493fbd031962a837d3336a0b21935607c3
Sherzybath <sdzyrsmy@gmail.com>
2024-08-28 02:43:05 +0530

    Bare

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const Nav = () => {
+const Nav = ({pare}) => {
   return (
-    <div>Nav</div>
+    <div className='NavBar'>
+        <div className='NavLeft'>SkillWeave</div>
+        <div className='NavMiddle'>
+            <button>About</button>
+            <button>FAQ</button>
+            <button>Team</button>
+            <button>Contact</button>
+        </div>
+        <div className='NavRight'>
+            <button onClick={() => pare(1)}>Sign Up</button>
+            <button onClick={() => pare(2)} className='ColorButton'>Sign in</button>
+        </div>
+    </div>
   )
 }
 
